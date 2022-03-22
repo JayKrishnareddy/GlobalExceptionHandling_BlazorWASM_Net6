0076d1b70a27e4a8708f657423652207aa50dc51
Jay Krishna Reddy <majji.jaya_krishna_reddy@nokia.com>
2022-03-22 16:33:28 +0530

    Handling Exception logs using Rest API's

---
+++ WebAPI/Models/Log.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAPI.Models
+{
+    public partial class Log
+    {
+        public int LogId { get; set; }
+        public string LogLevel { get; set; }
+        public string Source { get; set; }
+        public string ExceptionMessage { get; set; }
+        public string StackTrace { get; set; }
+        public string CreatedDate { get; set; }
+    }
+}
---
+++ WebAPI/Models/LoggingAPIContext.cs
@@ -0,0 +1,39 @@
+﻿namespace WebAPI.Models
+{
+    public partial class LoggingAPIContext : DbContext
+    {
+        public LoggingAPIContext()
+        {
+        }
+
+        public LoggingAPIContext(DbContextOptions<LoggingAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Log> Logs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Log>(entity =>
+            {
+                entity.Property(e => e.LogId).HasColumnName("log_id");
+
+                entity.Property(e => e.CreatedDate).HasColumnName("created_date");
+
+                entity.Property(e => e.ExceptionMessage).HasColumnName("exception_message");
+
+                entity.Property(e => e.LogLevel).HasColumnName("log_level");
+
+                entity.Property(e => e.Source).HasColumnName("source");
+
+                entity.Property(e => e.StackTrace).HasColumnName("stack_trace");
+
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
